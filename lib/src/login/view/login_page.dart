4d74b3889e88bf77256fba10cac301bdd7031c2f
gopiconceps-ship-it <gopiconceps@gmail.com>
2026-04-06 16:54:06 +0530

    leaderboard scroll bug fix

--- lib/src/login/view/login_page.dart
+++ lib/src/login/view/login_page.dart
@@ -23,47 +23,49 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           children: [
             Expanded(
-              child: Column(
-                children: [
-                  SizedBox(height: s(60)),
-                  /// Main Logo
-                  Image.asset(
-                    "assets/images/login/logo.webp",
-                    width: s(208),
-                    height: s(102),
-                  ),
-
-                  SizedBox(height: s(27)),
-
-                  /// Title
-                  Text(
-                    "Welcome Dealer",
-                    style: GoogleFonts.dmSans(
-                      fontSize: s(26),
-                      color: ColorPalette.whitetext,
-                      fontWeight: FontWeight.w700,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(height: s(60)),
+                    /// Main Logo
+                    Image.asset(
+                      "assets/images/login/logo.webp",
+                      width: s(208),
+                      height: s(102),
                     ),
-                  ),
-
-                  SizedBox(height: s(7)),
-
-                  /// Subtitle
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: s(40)),
-                    child: Text(
-                      "Manage your lottery business, tickets, and\ncustomers in one place",
-                      textAlign: TextAlign.center,
+                
+                    SizedBox(height: s(27)),
+                
+                    /// Title
+                    Text(
+                      "Welcome Dealer",
                       style: GoogleFonts.dmSans(
-                        fontSize: s(14),
-                          fontWeight: FontWeight.w400,
-                        color: ColorPalette.darktext,
+                        fontSize: s(26),
+                        color: ColorPalette.whitetext,
+                        fontWeight: FontWeight.w700,
                       ),
                     ),
-                  ),
-                   SizedBox(height: s(32)),
-
-                  //  LoginFormCard(scale: scale),
-                ],
+                
+                    SizedBox(height: s(7)),
+                
+                    /// Subtitle
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: s(40)),
+                      child: Text(
+                        "Manage your lottery business, tickets, and\ncustomers in one place",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.dmSans(
+                          fontSize: s(14),
+                            fontWeight: FontWeight.w400,
+                          color: ColorPalette.darktext,
+                        ),
+                      ),
+                    ),
+                     SizedBox(height: s(32)),
+                
+                    //  LoginFormCard(scale: scale),
+                  ],
+                ),
               ),
             ),
 
